beb169c5b148dff943a6992c40f065175af6eefe
Anthony STARCK <anthony.starck1@uqac.ca>
2018-04-27 09:57:03 -0400

    Login + Files

---
+++ StoreBox/Controllers/JWTInHeaderMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace StoreBox.Controllers
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class JWTInHeaderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JWTInHeaderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class JWTInHeaderMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<JWTInHeaderMiddleware>();
+        }
+    }
+}
